cb3f5d9842ae6c17df6c8e1010fd61bd02d5deec
zeinashaarawy <122227646+zeinashaarawy@users.noreply.github.com>
2025-11-18 20:18:22 +0200

    Delete Leaves-Subsystem/backend directory

--- Leaves-Subsystem/backend/src/modules/vacation-packages/dto/update-vacation-package.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateVacationPackageDto } from './create-vacation-package.dto';
-
-export class UpdateVacationPackageDto extends PartialType(
-  CreateVacationPackageDto,
-) {}
